063d9f92ffdef520d1b2e3a705762f5e92877119
brsvppv <borislav.popov@outlook.com>
2024-03-03 10:11:39 +0200

    Adding info in Convert-TunixTodateTime

--- DateTIme/Convert-UnixToDateTime.ps1
+++ DateTIme/Convert-UnixToDateTime.ps1
@@ -2,15 +2,21 @@ $not_before = 1709201598
 $expires_in = 86399
 $expires_on = 1709288298
 
+#Get Current time in unix format
+$CurrentTime = ([DateTimeOffset]$(Get-Date)).ToUnixTimeSeconds()
+Write-host $(Get-Date)
+Write-Host $CurrentTime
 
-$not_before = (Get-Date 01.01.1970).AddSeconds($tokenResponse.not_before)  
-$expires_in = (Get-Date).AddSeconds($tokenResponse.expires_in)
-$expires_on = (Get-Date).AddSeconds($tokenResponse.expires_on)
+# Method 1
+# $not_before = (Get-Date 01.01.1970).AddSeconds($not_before)  
+# $expires_in = (Get-Date).AddSeconds($expires_in)
+# $expires_on = (Get-Date).AddSeconds($expires_on)
+#Method 2
+$not_before = [DateTimeOffset]::FromUnixTimeSeconds($not_before).DateTime
+$expires_in = [DateTimeOffset]::FromUnixTimeSeconds($expires_in).DateTime
+$expires_on = [DateTimeOffset]::FromUnixTimeSeconds($expires_on).DateTime
 
-$not_before = [DateTimeOffset]::FromUnixTimeSeconds($tokenResponse.not_before).DateTime
-$expires_in = [DateTimeOffset]::FromUnixTimeSeconds($tokenResponse.expires_in).DateTime
-$expires_on = [DateTimeOffset]::FromUnixTimeSeconds($tokenResponse.expires_on).DateTime
-
+write-host $expires_on -ForegroundColor red
 $expires_in = 86399
 $days_left = [Math]::Ceiling($expires_in / 86400)
 Write-Host "Number of days left: $days_left"
