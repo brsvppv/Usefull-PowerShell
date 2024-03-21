be82d35fbf70a43fbcc1d70e3e96ac34f39ea50d
b.popov <b.popov@team-vision.bg>
2024-03-21 17:04:45 +0200

    improve downoad nssm function

--- CreateService/Download-NSSM.ps1
+++ CreateService/Download-NSSM.ps1
@@ -1,23 +1,26 @@
 Function Get-NSSM($Link) {
-    $WebResponse = Invoke-WebRequest "$Link/download" # | Where-Object { $_ -like '*.zip*' }
+    Write-host "Getting Web Response..."
+    $WebResponse = Invoke-WebRequest "$Link/download" -UseBasicParsing
     $ZipFiles = $WebResponse.Links |  Where-Object { $_ -like '*/release/*.zip*' }
-    
+    Write-host "Building Download Link"
     $downloadLink = $Link + $ZipFiles.href
     #$FileName = $download_link.Split("/")[-1]
     $fileName = [System.IO.Path]::GetFileName($downloadLink)
     #$download_zip = "{0}/{1}" -f $Link, $archives.href
     $userDownloads = (New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path
-    
     $filePath = Join-Path -Path $userDownloads -ChildPath $FileName
-
+    Write-host "Initiate Downloading...`nPlease Wait"
     Invoke-WebRequest -Uri $downloadLink -OutFile $filePath -ErrorAction Stop
+    Write-host "Download Finished"
     return $filePath
 
 }
 
 $file = Get-NSSM('https://nssm.cc/')
-
+$file 
+Write-Output "Extracting the archive to the current path"
 Expand-Archive -Path $file -DestinationPath $PSScriptRoot
-#Get Link in the HTML for realsed version
-# $ReleasedVersions = $WebResponse.Links
-# $ReleasedVersions
+Write-Output "Cleaning up...($file)"
+Remove-Item -Path $file -Force
+
+Pause
