a79c7fcd3af1b29f79fed5bf39ddfb1df7612848
b.popov <b.popov@team-vision.bg>
2024-04-01 12:08:39 +0300

    Combine Extract and Remove in the Download-NSSM Function

--- CreateService/Download-NSSM.ps1
+++ CreateService/Download-NSSM.ps1
@@ -1,26 +1,25 @@
 Function Get-NSSM($Link) {
-    Write-host "Getting Web Response..."
+    Write-Host "Getting Web Response..."
     $WebResponse = Invoke-WebRequest "$Link/download" -UseBasicParsing
     $ZipFiles = $WebResponse.Links |  Where-Object { $_ -like '*/release/*.zip*' }
-    Write-host "Building Download Link"
+    Write-Host "Building Download Link"
     $downloadLink = $Link + $ZipFiles.href
     #$FileName = $download_link.Split("/")[-1]
     $fileName = [System.IO.Path]::GetFileName($downloadLink)
     #$download_zip = "{0}/{1}" -f $Link, $archives.href
     $userDownloads = (New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path
     $filePath = Join-Path -Path $userDownloads -ChildPath $FileName
-    Write-host "Initiate Downloading...`nPlease Wait"
+    Write-Host "Initiate Downloading...`nPlease Wait"
     Invoke-WebRequest -Uri $downloadLink -OutFile $filePath -ErrorAction Stop
-    Write-host "Download Finished"
-    return $filePath
+    Write-Host "Download Finished"
+    #return $filePath
+    Write-Host "Extracting the archive to the current path"
+    Expand-Archive -Path $filePath -DestinationPath $PSScriptRoot
+    Write-Output "Cleaning up...($filePath)"
+    Remove-Item -Path $filePath -Force
 
 }
 
-$file = Get-NSSM('https://nssm.cc/')
-$file 
-Write-Output "Extracting the archive to the current path"
-Expand-Archive -Path $file -DestinationPath $PSScriptRoot
-Write-Output "Cleaning up...($file)"
-Remove-Item -Path $file -Force
+Get-NSSM('https://nssm.cc/')
 
-Pause
+
