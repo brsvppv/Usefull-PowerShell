3dfd9919bf505ac584fbaf00ed9e371a53e6a350
b.popov <b.popov@team-vision.bg>
2024-04-01 14:51:47 +0300

    more device info func

--- DevicesInfo/Get-Info.ps1
+++ DevicesInfo/Get-Info.ps1
@@ -4,3 +4,13 @@ Get-CimInstance -ClassName Win32_ComputerSystem
 
 # get device serialnu   
 Get-WmiObject Win32_BIOS | Select-Object SerialNumber
+
+
+#
+Get-WmiObject Win32_Processor | Select-Object NumberOfCores, NumberOfLogicalProcessors, LoadPercentage | Format-List
+
+# CPU load.
+Get-WmiObject Win32_Processor | Measure-Object -Property LoadPercentage -Average | Select-Object Average
+
+
+#WMIC CPU Get DeviceID,NumberOfCores,NumberOfLogicalProcessors,SocketDesignation
