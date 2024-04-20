957d42e02ef4cb548808e97220da964aea3e7304
Borislav G. Popov <65204241+brsvppv@users.noreply.github.com>
2024-04-20 20:53:17 +0300

    adding comment

--- Certificates/New-SelfSignedCert.ps1
+++ Certificates/New-SelfSignedCert.ps1
@@ -1,4 +1,5 @@
 Import-Module PKI -Verbose
+
 $MachineName = [System.Net.Dns]::GetHostByName($ENV:COMPUTERNAME).HostName
 
 New-SelfSignedCertificate -Type Custom -Subject "CN=$MachineName" `
@@ -12,10 +13,12 @@ New-SelfSignedCertificate -Type Custom -Subject "CN=$MachineName" `
     -HashAlgorithm "SHA256" `
     -CertStoreLocation "cert:\LocalMachine\My" `
     -NotAfter (Get-Date).AddYears(10)
+
+#Client Authentication + Server Authentication
 #-TextExtension "2.5.29.37={text}1.3.6.1.5.5.7.3.1, 1.3.6.1.5.5.7.3.2" `
 
-
 #-TextExtension These key usages have the following object identifiers:
+
 #Client Authentication: 1.3.6.1.5.5.7.3.2
 #Server Authentication: 1.3.6.1.5.5.7.3.1
 #Secure Email: 1.3.6.1.5.5.7.3.4
