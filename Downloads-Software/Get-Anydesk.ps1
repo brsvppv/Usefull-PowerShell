f9851eaddc518b8a5c5e82aa421fbc75ae3087a2
Borislav G. Popov <65204241+brsvppv@users.noreply.github.com>
2024-04-20 20:55:44 +0300

    change folder structure

--- Downloads-Software/Get-Anydesk.ps1
+++ Downloads-Software/Get-Anydesk.ps1
@@ -1,14 +1,21 @@
 Function Get-Anydesk() {
     param(
         $url = 'https://download.anydesk.com/AnyDesk.exe',
+
         $localPath = (Join-Path $env:ProgramData -ChildPath 'Anydesk'),
+
         $userDesktop = (New-Object -ComObject Shell.Application).NameSpace('shell:Desktop').Self.Path
     )
     if (Test-Path $localPath) { Remove-Item -LiteralPath $localPath -Force -Recurse }
+
     if (Test-Path $env:APPDATA\Anydesk) { Remove-Item -LiteralPath $env:APPDATA\Anydesk -Force -Recurse }
+    
     if (Test-Path "$userDesktop\Anydesk.lnk") { Remove-Item -Path "$userDesktop\Anydesk.lnk" }
+
     New-item -Path $localPath -ItemType Directory
+
     Invoke-WebRequest -Uri $url -OutFile $localPath\Anydesk.exe -ErrorAction Stop 
+    
     New-Item -ItemType SymbolicLink -Path  $userDesktop  -Name "Anydesk.lnk" -Value "$localPath\Anydesk.exe"
 }
 Get-Anydesk
